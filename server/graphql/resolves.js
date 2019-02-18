0362a7fa0a90b4cda215fec9cbc90f2a9ab3cc97
Henry Diamond <henrydiamond94@gmail.com>
2019-02-18 17:41:18 +0400

    User registreation and login

--- server/graphql/resolves.js
+++ server/graphql/resolves.js
@@ -1,4 +1,7 @@
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const { UserInputError, AuthenticationError } = require('apollo-server');
+const jwt = require('jsonwebtoken');
 
 const getUsers = async () => {
   try {
@@ -9,8 +12,85 @@ const getUsers = async () => {
   }
 };
 
+const register = async (_, args) => {
+  let errors = {};
+  let { username, email, password, confirmPassword } = args;
+  try {
+    // Fields Validation
+    if (email.trim() === '') errors.email = 'Email is required';
+    if (password.trim() === '') errors.password = 'Password is required';
+    if (username.trim() === '') errors.username = 'Username is required';
+    if (confirmPassword.trim() === '')
+      errors.confirmPassword = 'Confirm Password is required';
+    if (password !== confirmPassword)
+      errors.confirmPassword = 'Passwords do not match';
+
+    // Making sure user does not exist
+    let user = await User.findOne({ email: email }).exec();
+    if (user) errors.user = 'User already exist';
+
+    if (Object.keys(errors).length > 0) {
+      throw errors;
+    }
+
+    // New instance of user
+    user = new User({
+      username,
+      email,
+      password,
+    });
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+    await user.save();
+    return user;
+  } catch (err) {
+    console.log(err);
+    throw new UserInputError('Bad Input', { errors: err });
+  }
+};
+
+const login = async (_, args) => {
+  let errors = {};
+  let { email, password } = args;
+  try {
+    if (email.trim() === '') errors.email = 'Email must not be empty';
+    if (password.trim() === '') errors.password = 'Password must not be empty';
+
+    if (Object.keys(errors).length > 0) {
+      throw new UserInputError('Bad Input', { errors });
+    }
+    const user = await User.findOne({ email: email }).exec();
+
+    if (!user) {
+      errors.email = 'User does not exist';
+      throw new UserInputError('User does not exist', { errors });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      errors.password = 'password is incorrect';
+      throw new AuthenticationError('password is incorrect', { errors });
+    }
+
+    const token = jwt.sign({ email }, process.env.JWTSECRET, {
+      expiresIn: 60 * 60,
+    });
+    user.token = token;
+    return user;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
 module.exports = {
   Query: {
     getUsers,
+    login,
+  },
+  Mutation: {
+    register,
   },
 };
--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -3,13 +3,27 @@ const { gql } = require('apollo-server');
 module.exports = gql`
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  # This "User" type defines the queryable fields for every user in our data source.
+  # This "Book" type defines the queryable fields for every book in our data source.
   type User {
+    _id: ID!
     username: String!
     email: String!
+    token: String
   }
 
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
     getUsers: [User]!
+    login(email: String!, password: String!): User!
+  }
+  type Mutation {
+    register(
+      username: String!
+      email: String!
+      password: String!
+      confirmPassword: String!
+    ): User!
   }
 `;
